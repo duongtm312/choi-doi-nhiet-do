7324b7d7eec06296917670743baf91779ee1aee2
duongtm312 <103014462+duongtm312@users.noreply.github.com>
2022-04-12 11:27:56 +0700

    up

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputCelsius;
+
+
+inputCelsius = prompt("Enter the Celsius");
+
+let Celsius = parseInt(inputCelsius);
+let Fahrenheit = ((Celsius/5)*9)+32
+document.write("Fahrenheit is: " + Fahrenheit);
